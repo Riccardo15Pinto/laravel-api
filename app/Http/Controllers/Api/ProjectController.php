63d8ef39da5c47dfc9f6f957923938cb091a37a2
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-11 15:36:46 +0200

    set api projectcontroller index

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::with('type', 'technologys')->paginate(5);
 
         return response()->json($projects);
     }
