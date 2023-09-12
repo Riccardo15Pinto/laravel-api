0f7ec20e88ea49083b30283c5d6ea0f3f987f4ff
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-12 18:31:25 +0200

    fix function show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -31,7 +31,7 @@ class ProjectController extends Controller
      */
     public function show(string $id)
     {
-        $project = Project::find($id)->with('type', 'technologys')->first();
+        $project = Project::with('type', 'technologys')->find($id);
         if (!$project) return response(null, 404);
         return response()->json($project);
     }
