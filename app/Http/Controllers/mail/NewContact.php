55794763dd6ee41684f1e890e54197f50784c6a3
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-13 17:21:07 +0200

    set to line 35 in newcontactcontroller

--- app/Http/Controllers/mail/NewContact.php
+++ app/Http/Controllers/mail/NewContact.php
@@ -7,16 +7,31 @@ use App\Mail\NewContact as MailNewContact;
 use Illuminate\Http\Request;
 use App\Models\Contact;
 use Illuminate\Support\Facades\Mail;
-
+use Illuminate\Support\Facades\Validator;
 
 class NewContact extends Controller
 {
     public function email(Request $request)
     {
         $data = $request->all();
+        $validator = Validator::make($data, [
+            'email' => 'required|email',
+            'object' => 'required|string',
+            'content' => 'required|string',
+        ], [
+            'email.required' => 'L\'E-mail è obbligatoria',
+            'email.email' => 'L\'E-mail inserita non è valida',
+            'object.required' => 'L\' oggetto è obbligatorio',
+            'content.required' => 'Il contenuto dell\'E-mail è obbligatorio',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
         $new_contact = new Contact();
         $new_contact->email = $data['email'];
         $new_contact->save();
-        Mail::to("admin@example.com")->send(new MailNewContact($data['email'], $data['object'], $data['content']));
+        Mail::to($data['email'])->send(new MailNewContact($data['email'], $data['object'], $data['content']));
     }
 }
